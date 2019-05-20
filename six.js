b0db4d8e49484777060bc5ccec769f221d23354f
Quinn Freedman <quinnfreedman@gmail.com>
2019-05-20 16:51:50 -0400

    six

---
+++ six.js
@@ -0,0 +1,130 @@
+function six(rng) {
+    const NUM_POINTS = 30
+    const MARGIN = 200
+    const ACCELERATION = .1
+    const TARGET_FPS = 45
+    const SPAWN_RATE = .01
+    const CREEP_MARGIN = 60
+    const CREEP_SPEED = 0.6
+
+    const canvas = document.querySelector("canvas")
+    const ctx = canvas.getContext("2d")
+
+    const width = 800
+    const height = 600
+
+    canvas.width = width
+    canvas.height = height
+
+    ctx.fillStyle = "#fff"
+    ctx.fillRect(0, 0, width, height)
+
+    voronoi = new Voronoi()
+
+    let sites = range(NUM_POINTS).map(() => ({
+        x: Math.round(rng.nextRange(width)),
+        y: Math.round(rng.nextRange(height))
+    }))
+
+    let diagram = null
+
+    let velocity = []
+
+    let firstTime = true
+    startAnimating(draw, TARGET_FPS)
+    // draw()
+    function draw() {
+        canvas.width = width
+        // ctx.fillStyle = "#8addee"
+        ctx.fillStyle = "#000"
+        voronoi.recycle(diagram)
+
+        if (rng.nextFloat() < SPAWN_RATE || firstTime) {
+            firstTime = false
+            sites.push({ x: rng.normalRange(width), y: -MARGIN })
+            velocity.push(CREEP_SPEED)
+        }
+
+        let i = 0
+        while (i < velocity.length) {
+            let pointIndex = NUM_POINTS + i
+            if (sites[pointIndex].y > -CREEP_MARGIN) {
+                velocity[i] += ACCELERATION
+            }
+            sites[pointIndex].y += velocity[i]
+            if (sites[pointIndex].y > height + MARGIN) {
+                velocity.splice(i, 1)
+                sites.splice(pointIndex, 1)
+            } else {
+                i++
+            }
+        }
+
+
+        diagram = voronoi.compute(sites, {
+            xl: 0,
+            xr: width,
+            yt: 0,
+            yb: height
+        })
+
+        fillVoronoiDiagram(diagram)
+    }
+
+    function fillVoronoiDiagram(diagram) {
+        for (let cell of diagram.cells) {
+            let edges = cell.halfedges.map(he => he.edge)
+
+            let points = polyFromEdges(edges)
+            if (!points.length) continue
+
+            let center = [
+                points.map(({ x }) => x).reduce((a, b) => a + b) /
+                    points.length,
+                points.map(({ y }) => y).reduce((a, b) => a + b) / points.length
+            ]
+
+            points = points
+                .map(({ x, y }) => moveToward([x, y], center, 10))
+                .map(([x, y]) => ({ x, y }))
+
+            ctx.beginPath()
+            roundPolly(ctx, points, 20)
+            ctx.fill()
+        }
+        /**
+         * @param {Array} edges
+         */
+        function polyFromEdges(edges) {
+            if (!edges.length) return []
+            const pointsEqual = (a, b) =>
+                approxEqual(a.x, b.x) && approxEqual(a.y, b.y)
+
+            let edge1 = edges.pop()
+            let points = [edge1.va, edge1.vb]
+            while (edges.length) {
+                let point = points[points.length - 1]
+                let edgeToRemove = -1
+                for (let i of range(edges.length)) {
+                    let edge = edges[i]
+                    if (pointsEqual(point, edge.va)) {
+                        points.push(edge.vb)
+                        edgeToRemove = i
+                        break
+                    } else if (pointsEqual(point, edge.vb)) {
+                        points.push(edge.va)
+                        edgeToRemove = i
+                        break
+                    }
+                }
+                edges.splice(edgeToRemove, 1)
+            }
+
+            points.pop()
+            return points
+        }
+    }
+}
+
+//seed=111752166
+
--- util.js
+++ util.js
@@ -1,29 +1,29 @@
 function RNG(seed) {
     // LCG using GCC's constants
-    this.m = 0x80000000; // 2**31;
-    this.a = 1103515245;
-    this.c = 12345;
+    this.m = 0x80000000 // 2**31;
+    this.a = 1103515245
+    this.c = 12345
 
-    this.seed = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+    this.seed = seed ? seed : Math.floor(Math.random() * (this.m - 1))
 
     this.state = this.seed
 }
-RNG.prototype.nextInt = function () {
-    this.state = (this.a * this.state + this.c) % this.m;
-    return this.state;
+RNG.prototype.nextInt = function() {
+    this.state = (this.a * this.state + this.c) % this.m
+    return this.state
 }
 /**
  * returns in range [0,1]
  */
-RNG.prototype.nextFloat = function () {
-    return this.nextInt() / (this.m - 1);
+RNG.prototype.nextFloat = function() {
+    return this.nextInt() / (this.m - 1)
 }
 
-RNG.prototype.nextBool = function () {
-    return this.nextFloat() < .5
+RNG.prototype.nextBool = function() {
+    return this.nextFloat() < 0.5
 }
 
-RNG.prototype.nextSign = function () {
+RNG.prototype.nextSign = function() {
     return this.nextBool() ? 1 : -1
 }
 /**
@@ -31,40 +31,41 @@ RNG.prototype.nextSign = function () {
  * @param {number} start
  * @param {number} end
  */
-RNG.prototype.nextRange = function (start, end) {
+RNG.prototype.nextRange = function(start, end) {
     if (typeof end === "undefined") {
         end = start
         start = 0
     }
     // can't modulu nextInt because of weak randomness in lower bits
-    let rangeSize = end - start;
-    let randomUnder1 = this.nextInt() / this.m;
-    return start + Math.floor(randomUnder1 * rangeSize);
+    let rangeSize = end - start
+    let randomUnder1 = this.nextInt() / this.m
+    return start + Math.floor(randomUnder1 * rangeSize)
 }
-RNG.prototype.choice = function (array) {
-    return array[this.nextRange(0, array.length)];
+RNG.prototype.choice = function(array) {
+    return array[this.nextRange(0, array.length)]
 }
-RNG.prototype.normal = function (mean, stdev) {
+RNG.prototype.normal = function(mean, stdev) {
     if (typeof mean === "undefined") mean = 0
     if (typeof stdev === "undefined") stdev = 0
-    let u = 0, v = 0
+    let u = 0,
+        v = 0
     while (u === 0) u = this.nextFloat()
     while (v === 0) v = this.nextFloat()
     let random = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
-    return (random * stdev) + mean
+    return random * stdev + mean
 }
 /**
  * @param {number} start
  * @param {number} end
  */
-RNG.prototype.normalRange = function (start, end) {
+RNG.prototype.normalRange = function(start, end) {
     if (typeof end === "undefined") {
         end = start
         start = 0
     }
     let range = end - start
     let stdev = range / 6.0
-    let mean = start + (range / 2.0)
+    let mean = start + range / 2.0
     return clamp(start, end, this.normal(mean, stdev))
 }
 
@@ -106,9 +107,11 @@ function euclideanDistance(v1, v2) {
     if (v1.length !== v2.length) {
         throw "Vector lengths do not match"
     }
-    return Math.sqrt(range(v1.length)
-        .map((i) => (v1[i] - v2[i]) ** 2)
-        .reduce((a, b) => a + b))
+    return Math.sqrt(
+        range(v1.length)
+            .map(i => (v1[i] - v2[i]) ** 2)
+            .reduce((a, b) => a + b)
+    )
 }
 
 function setPixel(ctx, [x, y], color) {
@@ -121,8 +124,12 @@ function setPixel(ctx, [x, y], color) {
 }
 
 function lerpColor(colorA, colorB, t) {
-    if (typeof colorA === "string") { colorA = rgbToHex(colorA) }
-    if (typeof colorB === "string") { colorB = rgbToHex(colorB) }
+    if (typeof colorA === "string") {
+        colorA = rgbToHex(colorA)
+    }
+    if (typeof colorB === "string") {
+        colorB = rgbToHex(colorB)
+    }
 
     const [ar, ag, ab] = colorA
     const [br, bg, bb] = colorB
@@ -144,20 +151,20 @@ function isInt(n) {
 
 function rgbToHex(rgbOrHex) {
     if (typeof rgbOrHex === "string") {
-        let hex = rgbOrHex.replace('#', '');
-        let r = parseInt(hex.substring(0, 2), 16);
-        let g = parseInt(hex.substring(2, 4), 16);
-        let b = parseInt(hex.substring(4, 6), 16);
-        return [r, g, b].map((x) => x / 255)
+        let hex = rgbOrHex.replace("#", "")
+        let r = parseInt(hex.substring(0, 2), 16)
+        let g = parseInt(hex.substring(2, 4), 16)
+        let b = parseInt(hex.substring(4, 6), 16)
+        return [r, g, b].map(x => x / 255)
     } else if (Array.isArray(rgbOrHex)) {
         let r, g, b
         if (rgbOrHex.some(isFloat)) {
-            [r, g, b] = rgbOrHex.map((x) => Math.round(x * 255))
+            ;[r, g, b] = rgbOrHex.map(x => Math.round(x * 255))
         } else {
-            [r, g, b] = rgbOrHex
+            ;[r, g, b] = rgbOrHex
         }
 
-        const numberToHex = (c) => {
+        const numberToHex = c => {
             var hex = c.toString(16)
             return hex.length == 1 ? "0" + hex : hex
         }
@@ -166,11 +173,10 @@ function rgbToHex(rgbOrHex) {
     }
 }
 
-
 function moveInDirection(from, theta, distance) {
     return [
         from[0] + Math.cos(theta) * distance,
-        from[1] + Math.sin(theta) * distance,
+        from[1] + Math.sin(theta) * distance
     ]
 }
 
@@ -181,8 +187,84 @@ function lerp(a, b, x) {
 function moveToward(from, to, distance) {
     let totalDistance = euclideanDistance(from, to)
     let ratio = distance / totalDistance
-    return [
-        lerp(from[0], to[0], ratio),
-        lerp(from[1], to[1], ratio)
-    ]
+    return [lerp(from[0], to[0], ratio), lerp(from[1], to[1], ratio)]
+}
+
+function approxEqual(v1, v2, epsilon) {
+    if (epsilon == null) {
+        epsilon = 0.001
+    }
+    return Math.abs(v1 - v2) < epsilon
+}
+
+/**
+ * Draws a polygon with rounded corners
+ * @param {CanvasRenderingContext2D} ctx The canvas context
+ * @param {Array} points A list of `{x, y}` points
+ * @radius {number} how much to round the corners
+ */
+function roundPolly(ctx, points, radius) {
+    function distance(p1, p2) {
+        return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
+    }
+
+    function lerp2D(p1, p2, t) {
+        return {
+            x: lerp(p1.x, p2.x, t),
+            y: lerp(p1.y, p2.y, t)
+        }
+    }
+
+    const numPoints = points.length
+
+    let corners = []
+    for (let i of range(numPoints)) {
+        let lastPoint = points[i]
+        let thisPoint = points[(i + 1) % numPoints]
+        let nextPoint = points[(i + 2) % numPoints]
+
+        let lastEdgeLength = distance(lastPoint, thisPoint)
+        let lastOffsetDistance = Math.min(lastEdgeLength / 2, radius)
+        let start = lerp2D(
+            thisPoint,
+            lastPoint,
+            lastOffsetDistance / lastEdgeLength
+        )
+
+        let nextEdgeLength = distance(nextPoint, thisPoint)
+        let nextOffsetDistance = Math.min(nextEdgeLength / 2, radius)
+        let end = lerp2D(
+            thisPoint,
+            nextPoint,
+            nextOffsetDistance / nextEdgeLength
+        )
+
+        corners.push([start, thisPoint, end])
+    }
+
+    ctx.moveTo(corners[0][0].x, corners[0][0].y)
+    for (let [start, ctrl, end] of corners) {
+        ctx.lineTo(start.x, start.y)
+        ctx.quadraticCurveTo(ctrl.x, ctrl.y, end.x, end.y)
+    }
+
+    ctx.closePath()
+}
+
+function startAnimating(callback, fps) {
+    let fpsInterval = 1000 / fps
+    let then = Date.now()
+
+    function animate() {
+        requestAnimationFrame(animate)
+        now = Date.now()
+        elapsed = now - then
+
+        if (elapsed > fpsInterval) {
+            then = now - (elapsed % fpsInterval)
+            callback()
+        }
+    }
+
+    animate()
 }
