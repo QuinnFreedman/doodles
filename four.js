7adc29136608d8df0e412031268e4f0348a85851
Quinn Freedman <quinnfreedman@gmail.com>
2019-05-20 00:49:14 -0400

    four

--- four.js
+++ four.js
@@ -1,5 +1,8 @@
 function four(rng) {
     const NUM_POINTS = 30
+    const FREQ = 8
+    const AMPLITUDE = 6
+    const CYCLE_SPREAD = (Math.PI * 2) / 3
 
     const canvas = document.querySelector("canvas")
     const ctx = canvas.getContext("2d")
@@ -28,15 +31,8 @@ function four(rng) {
     }
 
     let offsets = range(sites.length).map(() =>
-        rng.nextRange(0, (Math.PI * 2) / 3)
+        rng.normalRange(0, CYCLE_SPREAD)
     )
-
-    // ctx.fillStyle = "#f00"
-    // for (let {x, y} of sites) {
-    //     ctx.beginPath()
-    //     ctx.arc(x, y, 5, 0, Math.PI * 255)
-    //     ctx.fill()
-    // }
 
     let t = 0
     let diagram = null
@@ -48,7 +44,7 @@ function four(rng) {
         ctx.fillStyle = "#8addee"
         t++
         for (let i of range(sites.length)) {
-            sites[i].x = initialSites[i].x + Math.sin(offsets[i] + t / 8) * 4
+            sites[i].x = initialSites[i].x + Math.sin(offsets[i] + t / FREQ) * AMPLITUDE 
         }
         voronoi.recycle(diagram)
         diagram = voronoi.compute(sites, {
@@ -58,15 +54,16 @@ function four(rng) {
             yb: height
         })
 
-
         for (let cell of diagram.cells) {
-            let edges =cell.halfedges.map((he) => he.edge) 
+            let edges = cell.halfedges.map(he => he.edge)
 
             let points = polyFromEdges(edges)
-            
+            if (!points.length) continue
+
             let center = [
-                points.map(({x}) => x).reduce((a, b) => a + b) / points.length,
-                points.map(({y}) => y).reduce((a, b) => a + b) /points.length 
+                points.map(({ x }) => x).reduce((a, b) => a + b) /
+                    points.length,
+                points.map(({ y }) => y).reduce((a, b) => a + b) / points.length
             ]
 
             points = points
@@ -74,9 +71,6 @@ function four(rng) {
                 .map(([x, y]) => ({ x, y }))
 
             ctx.beginPath()
-            // ctx.moveTo(points[0].x, points[0].y)
-            // points.forEach(p => ctx.lineTo(p.x, p.y))
-            // ctx.closePath()
             roundPolly(ctx, points, 20)
             ctx.fill()
         }
@@ -86,6 +80,7 @@ function four(rng) {
      * @param {Array} edges
      */
     function polyFromEdges(edges) {
+        if (!edges.length) return []
         const pointsEqual = (a, b) =>
             approxEqual(a.x, b.x) && approxEqual(a.y, b.y)
 
@@ -113,103 +108,64 @@ function four(rng) {
         return points
     }
 }
+
 function approxEqual(v1, v2, epsilon) {
     if (epsilon == null) {
         epsilon = 0.001
     }
     return Math.abs(v1 - v2) < epsilon
 }
+
 /**
- * From https://riptutorial.com/html5-canvas/example/18766/render-a-rounded-polygon-
+ * Draws a polygon with rounded corners
+ * @param {CanvasRenderingContext2D} ctx The canvas context
+ * @param {Array} points A list of `{x, y}` points
+ * @radius {number} how much to round the corners
  */
 function roundPolly(ctx, points, radius) {
-    var i,
-        x,
-        y,
-        len,
-        p1,
-        p2,
-        p3,
-        v1,
-        v2,
-        sinA,
-        sinA90,
-        radDirection,
-        drawDirection,
-        angle,
-        halfAngle,
-        cRadius,
-        lenOut
-    var asVec = function(p, pp, v) {
-        // convert points to a line with len and normalised
-        v.x = pp.x - p.x // x,y as vec
-        v.y = pp.y - p.y
-        v.len = Math.sqrt(v.x * v.x + v.y * v.y) // length of vec
-        v.nx = v.x / v.len // normalised
-        v.ny = v.y / v.len
-        v.ang = Math.atan2(v.ny, v.nx) // direction of vec
+    function distance(p1, p2) {
+        return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
     }
-    v1 = {}
-    v2 = {}
-    len = points.length // number points
-    p1 = points[len - 1] // start at end of path
-    for (i = 0; i < len; i++) {
-        // do each corner
-        p2 = points[i % len] // the corner point that is being rounded
-        p3 = points[(i + 1) % len]
-        // get the corner as vectors out away from corner
-        asVec(p2, p1, v1) // vec back from corner point
-        asVec(p2, p3, v2) // vec forward from corner point
-        // get corners cross product (asin of angle)
-        sinA = v1.nx * v2.ny - v1.ny * v2.nx // cross product
-        // get cross product of first line and perpendicular second line
-        sinA90 = v1.nx * v2.nx - v1.ny * -v2.ny // cross product to normal of line 2
-        angle = Math.asin(sinA) // get the angle
-        radDirection = 1 // may need to reverse the radius
-        drawDirection = false // may need to draw the arc anticlockwise
-        // find the correct quadrant for circle center
-        if (sinA90 < 0) {
-            if (angle < 0) {
-                angle = Math.PI + angle // add 180 to move us to the 3 quadrant
-            } else {
-                angle = Math.PI - angle // move back into the 2nd quadrant
-                radDirection = -1
-                drawDirection = true
-            }
-        } else {
-            if (angle > 0) {
-                radDirection = -1
-                drawDirection = true
-            }
+
+    function lerp2D(p1, p2, t) {
+        return {
+            x: lerp(p1.x, p2.x, t),
+            y: lerp(p1.y, p2.y, t)
         }
-        halfAngle = angle / 2
-        // get distance from corner to point where round corner touches line
-        lenOut = Math.abs((Math.cos(halfAngle) * radius) / Math.sin(halfAngle))
-        if (lenOut > Math.min(v1.len / 2, v2.len / 2)) {
-            // fix if longer than half line length
-            lenOut = Math.min(v1.len / 2, v2.len / 2)
-            // ajust the radius of corner rounding to fit
-            cRadius = Math.abs(
-                (lenOut * Math.sin(halfAngle)) / Math.cos(halfAngle)
-            )
-        } else {
-            cRadius = radius
-        }
-        x = p2.x + v2.nx * lenOut // move out from corner along second line to point where rounded circle touches
-        y = p2.y + v2.ny * lenOut
-        x += -v2.ny * cRadius * radDirection // move away from line to circle center
-        y += v2.nx * cRadius * radDirection
-        // x,y is the rounded corner circle center
-        ctx.arc(
-            x,
-            y,
-            cRadius,
-            v1.ang + (Math.PI / 2) * radDirection,
-            v2.ang - (Math.PI / 2) * radDirection,
-            drawDirection
-        ) // draw the arc clockwise
-        p1 = p2
-        p2 = p3
     }
+
+    const numPoints = points.length
+
+    let corners = []
+    for (let i of range(numPoints)) {
+        let lastPoint = points[i]
+        let thisPoint = points[(i + 1) % numPoints]
+        let nextPoint = points[(i + 2) % numPoints]
+
+        let lastEdgeLength = distance(lastPoint, thisPoint)
+        let lastOffsetDistance = Math.min(lastEdgeLength / 2, radius)
+        let start = lerp2D(
+            thisPoint,
+            lastPoint,
+            lastOffsetDistance / lastEdgeLength
+        )
+
+        let nextEdgeLength = distance(nextPoint, thisPoint)
+        let nextOffsetDistance = Math.min(nextEdgeLength / 2, radius)
+        let end = lerp2D(
+            thisPoint,
+            nextPoint,
+            nextOffsetDistance / nextEdgeLength
+        )
+
+        corners.push([start, thisPoint, end])
+    }
+
+    ctx.moveTo(corners[0][0].x, corners[0][0].y)
+    for (let [start, ctrl, end] of corners) {
+        ctx.lineTo(start.x, start.y)
+        ctx.quadraticCurveTo(ctrl.x, ctrl.y, end.x, end.y)
+    }
+
     ctx.closePath()
 }
