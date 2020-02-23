03d4f50c064229ae6d64cbe2a4c94adcbe38f075
Quinn Freedman <quinnfreedman@gmail.com>
2020-02-23 12:48:10 -0800

    removed thirtyone

--- graveyard/thirtyone.js
+++ graveyard/thirtyone.js
@@ -11,30 +11,48 @@ function thirtyone(rng) {
     ctx.fillStyle = "#fff"
     ctx.fillRect(0, 0, WIDTH, HEIGHT)
 
-    const SMALLEST_SPLIT = 20
+    const SMALLEST_SPLIT = 10 // 20
     const NUM_SPLIT_RANGE = [2, 4]
 
-    ctx.strokeStyle = "#000"
 
-    function paintRect(x, y, w, h) {
-        ctx.strokeRect(x, y, w, h)
+    const PALETTE = [
+        "#9DA8A1",
+        "#606D5D",
+        "#BC9CB0",
+        "#D3CDD7",
+        "#DDF2EB"
+    ]
+
+    function paintRect(x, y, w, h, color, oldSplitDirection) {
         const numSplits = Math.min(w, h) > SMALLEST_SPLIT
             ? rng.nextRange(...NUM_SPLIT_RANGE)
             : 0
-        const splitDirection = rng.nextBool()
+        if (numSplits === 0) {
+            let alpha = rng.choice(["ff", "fb", "f8", "f4", "f0"])
+            ctx.fillStyle = color + alpha
+            ctx.strokeStyle = color + alpha
+            ctx.fillRect(x, y, w + 1, h + 1)
+            //ctx.strokeRect(x, y, w, h)
+        }
+        const splitDirection = rng.nextBool(/* w / h */)
+        
+        if (splitDirection != oldSplitDirection) {
+            color = rng.choice(PALETTE)
+        }
+
         for (let i = 0; i < numSplits; i++) {
             if (splitDirection) {
                 let _width = w / numSplits
                 let _height = h
                 let _x = x + i * _width
                 let _y = y
-                paintRect(_x, _y, _width, _height)
+                paintRect(_x, _y, _width, _height, color, splitDirection)
             } else {
                 let _width = w
                 let _height = h / numSplits
                 let _x = x
                 let _y = y + i * _height
-                paintRect(_x, _y, _width, _height)
+                paintRect(_x, _y, _width, _height, color, splitDirection)
             }
         }
     }
